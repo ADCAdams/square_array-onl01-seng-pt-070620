1d3ab06c2f10c49e322c3000db88fa6ff4d2c54d
Andy Adams <andrewdcadams@gmail.com>
2020-07-08 17:56:43 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,6 +1,12 @@
 def square_array(array)
   newArray = []
+  counter = 0 
   
-  
+  while counter < array.length 
+    newNum = (array[counter] * array[counter])
+    newArray.push(newNum)
+    counter += 1 
+  end
+  return newArray
   # your code here
 end
